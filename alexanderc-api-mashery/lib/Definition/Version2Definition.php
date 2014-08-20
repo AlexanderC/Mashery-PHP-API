4b55c597c2471ad1a076eb5d2718e27267b21015
AlexanderC <self@alexanderc.me>
2014-08-20 16:25:17 +0300

    update mastery objects

--- alexanderc-api-mashery/lib/Definition/Version2Definition.php
+++ alexanderc-api-mashery/lib/Definition/Version2Definition.php
@@ -75,7 +75,8 @@ class Version2Definition implements DefinitionInterface
         return [
             'member', 'application', 'key',
             'package_key', 'package', 'plan',
-            'service', 'role', 'developer_class'
+            'service', 'role', 'developer_class',
+            'plan_endpoint'
         ];
     }
 }
